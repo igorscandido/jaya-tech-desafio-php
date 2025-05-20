ad1f0869ef04a2516ef56ddb45d1f55d0d707b57
ext-igcandid_meli <ext_igcandid@mercadolivre.com>
2025-05-19 22:25:37 -0300

    instantiated api routes for all resources

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\RideController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::post('/drivers', [DriverController::class, 'store']);
+Route::delete('/drivers/{id}', [DriverController::class, 'destroy']);
+Route::get('/drivers/{id}/get-rides', [DriverController::class, 'getOpenRides']);
+
+Route::get('/rides/{id}', [RideController::class, 'show']);
+Route::delete('/rides/{id}', [RideController::class, 'destroy']);
+Route::post('/rides/request-driver', [RideController::class, 'requestDriver']);
+Route::post('/rides/cancel-ride', [RideController::class, 'cancelRide']);
+Route::post('/rides/accept-ride', [RideController::class, 'acceptRide']);
+Route::post('/rides/refuse-ride', [RideController::class, 'refuseRide']);
+Route::post('/rides/finish-ride', [RideController::class, 'finishRide']);
